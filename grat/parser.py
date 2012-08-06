65ac069a008d4cc4b2edf1bc6a4f0ef386769fb6
tactics74 <tactics74@yahoo.com>
2012-08-06 15:39:27 -0700

    Fixed all major bugs for parser

--- grat/parser.py
+++ grat/parser.py
@@ -18,33 +18,46 @@ class ArgumentError(Exception):
 class Element(object):
     """Object to hold an HTML element."""
     def __init__(self, content):
-        if type(content) == list:
-            #assumes tags are in order
-            self.start_tag = content[0]
-            self.closing_tag = content[-1]
-            self.content = content[1:-1]
-
-        if type(content) == str:
-            if not value.startswith('<'):
-                raise ArgumentError
-            if not value.endswith('>'):
-                raise ArgumentError
-
-            self.start_tag = content[:content.find('>')]
-            self.closing_tag = content[content.find("</"):]
-            self.content = content[content.find('>'):
-                                   content.find("</")]
-
-
-        if self.value.count('<') > 1:
-            self.content = self.remove_tags()
-            self.tag_type = self.get_tag_type()
-        else:
+        if content == None:
             self.content = None
-            self.tag_type = None
+        else:
+            if type(content) == list:
+                #assumes tags are in order
+                self.start_tag = content[0]
+                self.closing_tag = content[-1]
+                self.content = content[1:-1]
+                self.tag_type = self.closing_tag[2:-1].split()[0]
+
+            if type(content) == str:
+                if not content.startswith('<'):
+                    raise ArgumentError
+                if not content.endswith('>'):
+                    raise ArgumentError
+
+                self.start_tag = content[:content.find('>')]
+                self.closing_tag = content[content.find("</"):]
+                self.content = content[content.find('>'):
+                                       content.find("</")]
+                self.tag_type = self.closing_tag[2:-1].split()[0]
+
+    def startswith(self, str):
+        if self.content != None:
+            return self.content.startswith(str)
+        return -1
+
+    def expand(self):
+        """Returns a string that contains the expanded version of self.content.
+        By expanded version we mean that all element objects inside 
+        self.content will be in there string form."""
+        pass
+
+    def print_stats(self):
+        print "Start tag: ", str(self.start_tag)
+        print "Closing tag: ", str(self.closing_tag)
+        print "Content: ", str(self.content)
 
     def __str__(self):
-        return self.content
+        return str(self.start_tag) + str(self.content) + str(self.closing_tag)
 
 
 class Page(object):
@@ -66,43 +79,54 @@ class Page(object):
             print "Error: ", self.name, " has a null value."
 
         prev_tags = []
-        future_tags = find_all_tags(self.value)
+        future_tags = self.find_all_tags(self.value)
         for each_tag in future_tags:
             if each_tag.startswith("</"):
                 curr_element_tags = []
                 while True:
                     popped_tag = prev_tags.pop()
-                    curr_element_tags.append(popped_tag.reverse())
-                    if find_tag_type(popped_tag) == find_tag_type(each_tag):
-                        prev_tags.append( Element(curr_element_tags) )
+                    curr_element_tags.append(popped_tag)
+                    if self.find_tag_type(popped_tag) == \
+                       self.find_tag_type(each_tag):
+                        curr_element_tags.reverse()
+                        curr_element_tags.append(each_tag)
+
+                        curr_element = Element(curr_element_tags)
+                        if curr_element != None: 
+                            prev_tags.append( curr_element )
                         break
 
             else:
                 prev_tags.append(each_tag)
 
-        self.tags = prev_tags
+        self.html = prev_tags[0]
 
-    def find_all_tags(stryng):
+    def find_all_tags(self, stryng):
         """Returns all tags in a list"""
-       tags = [] 
-       while True:
-            if stryng == "":
+        tags = [] 
+        while True:
+            if stryng.strip() == '':
                 break
             index = stryng.find('<')
-            
+
             if index > stryng.find('>'):
                 index = stryng.find('>') + 1
 
             if index == 0:
                 index = stryng.find('>') + 1
-                
-            tags.append(stryng[:index])
+            
+            if stryng[:index].strip() != '':
+                tags.append(stryng[:index].strip())
+
             stryng = stryng[index:]
-       return tags
+        return tags
        
 
-    def find_tag_type(tag):
+    def find_tag_type(self, tag):
         """Returns tag stripped of angle brackets and forward slash"""
+        if type(tag) == Element:
+            return None
+
         if tag.startswith('</'):
             return tag[2:-1].split()[0]
         
--- test.py
+++ test.py
@@ -6,9 +6,8 @@ from grat import *
 from urllib2 import *
 
 def main():
-    webpage = parser.Page("http://www.python.org")
-    for tag in webpage.tags:
-        print tag, "\n\n"
+    webpage = parser.Page("http://localhost/etc/parsetest.html")
+    print webpage.html.content[1].content[1]
 
 if __name__ == "__main__":
     main()
